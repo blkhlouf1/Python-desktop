bdb48f375323887bcc50060fb9cfd77e14046e58
blkhlouf1 <bilal.lkhlouf@gmail.com>
2021-11-15 10:52:46 +0100

    code updated

--- threa.py
+++ threa.py
@@ -15,24 +15,39 @@ def callback():
 
 #Create Input ********************
 
-start_time = time.time()
-data = ['a','b','v','d','e','f','g','h']
-arr = []
-counter = 0
+start_time = time.time()from multiprocessing import Pool
+import subprocess
+import time
+from multiprocessing.pool import ThreadPool
+import random
+import threading
+import time
 
-maxconnections = 2
-# ...
-pool_sema = threading.BoundedSemaphore(value=maxconnections)
-print(pool_sema)
-with pool_sema:
-    
-    try:
-        threading.Thread(target=callback).start()
-        pool_sema.release()
-    finally:
-        print("--- %s seconds ---" % (time.time() - start_time))
-        
+MAX_THREADS = 15
+print_lock = threading.Lock()
 
 
-input('done.')
-exit()
+def open_profile(profilename):
+		strin = '"C:\Program Files\Cyberfox\Cyberfox.exe"'+' -p ' + profilename + " -no-remote" + " imacros://run/?m=saber.js"
+		subprocess.run(strin)
+		time.sleep(10)
+
+
+
+
+def export(profilename):
+    pool = ThreadPool(processes=MAX_THREADS)
+    pool.map_async(open_profile, profilename)
+    pool.close()
+    pool.join()  # block until all threads exit
+
+def main():
+	profiles = []
+	for i in range (1,600):
+		profile_name = "su"+ str(i)
+		profiles.append(profile_name)
+	print(profiles)
+	export(profiles)
+
+if __name__ == "__main__":
+    main()
